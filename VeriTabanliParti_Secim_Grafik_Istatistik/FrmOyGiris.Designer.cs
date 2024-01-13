7b52c1be1867c100f384d22dc3f7249abc58e983
Ece Kahraman <ecek.iletisim@gmail.com>
2024-01-13 19:13:42 +0300

    VeriTabanliParti_Secim_Grafik_Istatistik
     Bölge ekleme, Parti oy ekleme, Bölgelere göre seçim grafikleri.
    Veri tabanı için  Microsoft SQL Server kullanılmıştır.

---
+++ VeriTabanliParti_Secim_Grafik_Istatistik/FrmOyGiris.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace VeriTabanliParti_Secim_Grafik_Istatistik
+{
+    partial class VTGS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtIlceAd = new System.Windows.Forms.TextBox();
+            this.TxtA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtD = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtE = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnOyGırıs = new System.Windows.Forms.Button();
+            this.BtnÇıkış = new System.Windows.Forms.Button();
+            this.BtnGrafık = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İLÇE AD:";
+            // 
+            // TxtIlceAd
+            // 
+            this.TxtIlceAd.Location = new System.Drawing.Point(163, 58);
+            this.TxtIlceAd.Name = "TxtIlceAd";
+            this.TxtIlceAd.Size = new System.Drawing.Size(277, 34);
+            this.TxtIlceAd.TabIndex = 1;
+            // 
+            // TxtA
+            // 
+            this.TxtA.Location = new System.Drawing.Point(204, 128);
+            this.TxtA.Name = "TxtA";
+            this.TxtA.Size = new System.Drawing.Size(195, 34);
+            this.TxtA.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A PARTİ:";
+            // 
+            // TxtB
+            // 
+            this.TxtB.Location = new System.Drawing.Point(204, 190);
+            this.TxtB.Name = "TxtB";
+            this.TxtB.Size = new System.Drawing.Size(195, 34);
+            this.TxtB.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "B PARTİ:";
+            // 
+            // TxtC
+            // 
+            this.TxtC.Location = new System.Drawing.Point(204, 252);
+            this.TxtC.Name = "TxtC";
+            this.TxtC.Size = new System.Drawing.Size(195, 34);
+            this.TxtC.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "C PARTİ:";
+            // 
+            // TxtD
+            // 
+            this.TxtD.Location = new System.Drawing.Point(204, 312);
+            this.TxtD.Name = "TxtD";
+            this.TxtD.Size = new System.Drawing.Size(195, 34);
+            this.TxtD.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(107, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "D PARTİ:";
+            // 
+            // TxtE
+            // 
+            this.TxtE.Location = new System.Drawing.Point(204, 374);
+            this.TxtE.Name = "TxtE";
+            this.TxtE.Size = new System.Drawing.Size(195, 34);
+            this.TxtE.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(107, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "E PARTİ:";
+            // 
+            // BtnOyGırıs
+            // 
+            this.BtnOyGırıs.BackColor = System.Drawing.Color.PaleGreen;
+            this.BtnOyGırıs.Location = new System.Drawing.Point(204, 445);
+            this.BtnOyGırıs.Name = "BtnOyGırıs";
+            this.BtnOyGırıs.Size = new System.Drawing.Size(195, 38);
+            this.BtnOyGırıs.TabIndex = 12;
+            this.BtnOyGırıs.Text = "OY GİRİŞİ YAP";
+            this.BtnOyGırıs.UseVisualStyleBackColor = false;
+            this.BtnOyGırıs.Click += new System.EventHandler(this.BtnOyGırıs_Click);
+            // 
+            // BtnÇıkış
+            // 
+            this.BtnÇıkış.BackColor = System.Drawing.Color.Coral;
+            this.BtnÇıkış.Location = new System.Drawing.Point(276, 532);
+            this.BtnÇıkış.Name = "BtnÇıkış";
+            this.BtnÇıkış.Size = new System.Drawing.Size(123, 36);
+            this.BtnÇıkış.TabIndex = 13;
+            this.BtnÇıkış.Text = "ÇIKIŞ YAP";
+            this.BtnÇıkış.UseVisualStyleBackColor = false;
+            // 
+            // BtnGrafık
+            // 
+            this.BtnGrafık.BackColor = System.Drawing.Color.Khaki;
+            this.BtnGrafık.Location = new System.Drawing.Point(204, 489);
+            this.BtnGrafık.Name = "BtnGrafık";
+            this.BtnGrafık.Size = new System.Drawing.Size(195, 36);
+            this.BtnGrafık.TabIndex = 14;
+            this.BtnGrafık.Text = "GRAFİKLER";
+            this.BtnGrafık.UseVisualStyleBackColor = false;
+            this.BtnGrafık.Click += new System.EventHandler(this.BtnGrafık_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(98, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(443, 28);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Seçim İstatistik ve Grafik Sistemine Hoşgeldiniz";
+            // 
+            // VTGS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(553, 600);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.BtnGrafık);
+            this.Controls.Add(this.BtnÇıkış);
+            this.Controls.Add(this.BtnOyGırıs);
+            this.Controls.Add(this.TxtE);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtIlceAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "VTGS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VTGS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtIlceAd;
+        private System.Windows.Forms.TextBox TxtA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtE;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnOyGırıs;
+        private System.Windows.Forms.Button BtnÇıkış;
+        private System.Windows.Forms.Button BtnGrafık;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
